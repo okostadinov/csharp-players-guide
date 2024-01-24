90cc87a8b8280f5948c57d1a2d1db97c933531d0
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-01-24 15:53:18 +0200

    finish the point

---
+++ the-point/Program.cs
@@ -0,0 +1,25 @@
+﻿Point first = new Point(2, 3);
+Point second = new Point(-4, 0);
+Point third = new Point();
+System.Console.WriteLine($"First Point:\t{first.Coordinates}");
+System.Console.WriteLine($"Second Point:\t{second.Coordinates}");
+System.Console.WriteLine($"Third Point:\t{third.Coordinates}");
+
+class Point
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public string Coordinates => $"({X}, {Y})";
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+}
