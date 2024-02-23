8c287ba089d1ba38706855d99ea7facb00360fb4
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-02-23 12:26:01 +0200

    finish the long game

---
+++ the-long-game/Program.cs
@@ -0,0 +1,19 @@
+﻿int score = 0;
+Console.Write("Please enter your name: ");
+string? name = Console.ReadLine();
+
+string filename = name + ".txt";
+if (File.Exists(filename)) {
+    if (int.TryParse(File.ReadAllText(filename), out int oldScore)) {
+        score += oldScore;
+    }
+}
+
+Console.WriteLine($"Current score: {score}. You can start typing.");
+while (Console.ReadKey().Key != ConsoleKey.Enter) {
+    score++;
+    Console.WriteLine(score);
+}
+
+Console.WriteLine($"Final score: {score}.");
+File.WriteAllText(filename, score.ToString());
