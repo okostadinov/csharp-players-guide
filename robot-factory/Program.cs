526244d6f9fe2ffa2256a3b61d327ffbfdbf59ab
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-02-24 15:13:43 +0200

    finish robot factory

---
+++ robot-factory/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Dynamic;
+
+int count = 1;
+while (true)
+{
+    dynamic robot = new ExpandoObject();
+    robot.ID = 1;
+
+    Console.Write("Does the robot have a name (yes/no)? ");
+    string? input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.Write("Enter a name: ");
+        robot.Name = Console.ReadLine();
+    }
+
+    Console.Write("Does the robot have a specific size (yes/no)? ");
+    input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.Write("Enter height: ");
+        robot.Height = Console.ReadLine();
+        Console.Write("Enter width: ");
+        robot.Width = Console.ReadLine();
+    }
+
+    Console.Write("Does the robot have a specific color (yes/no)? ");
+    input = Console.ReadLine();
+    if (input == "yes")
+    {
+        Console.Write("Enter color: ");
+        robot.Color = Console.ReadLine();
+    }
+
+    DisplayRobotCharacteristics(robot);
+    count++;
+}
+
+void DisplayRobotCharacteristics(dynamic robot)
+{
+    var robotDictionary = (IDictionary<string, object>)robot;
+
+    foreach (string key in robotDictionary.Keys)
+        Console.WriteLine($"{key}: {robotDictionary[key]}");
+}
