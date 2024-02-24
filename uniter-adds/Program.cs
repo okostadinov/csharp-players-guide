b46759a8b0669e02b00aac948e3fc2a7bf597250
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-02-24 14:56:18 +0200

    finish uniter of adds

---
+++ uniter-adds/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine(Add(2, 3));
+Console.WriteLine(Add(2.5, 3.5));
+Console.WriteLine(Add("Hello", "again"));
+Console.WriteLine(Add(DateTime.Now, TimeSpan.FromSeconds(30)));
+
+dynamic Add(dynamic a, dynamic b) => a + b;
